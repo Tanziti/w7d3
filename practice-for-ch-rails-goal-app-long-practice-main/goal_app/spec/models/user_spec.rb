51c6d557cfdeb855f537700ef05954094e96a908
Dennis Lee <dennislee1499@gmail.com>
2023-08-09 14:46:12 -0400

    finished specs for password=method

--- practice-for-ch-rails-goal-app-long-practice-main/goal_app/spec/models/user_spec.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/goal_app/spec/models/user_spec.rb
@@ -19,11 +19,30 @@ RSpec.describe User, type: :model do
         expect(zi.username).to eq(user.username)
         expect(zi.password_digest).to eq(user.password_digest)
       end
-
-      
     end
 
+    context "with an invalid username and password" do 
+      it "returns nil" do 
+        dennis = User.create(username: "dennis", password: "password")
+        user = User.find_by_credentials("dennis", "zzzzz")
+        expect(user).to be_nil
+      end
+    end
+  end
 
+  describe "password encryption" do 
+    it "does not save passwords to the database" do 
+      User.create!(username: "mary", password: "lamb")
+      mary = User.find_by(username: "mary")
+      expect(mary.password).not_to be("lamb")
+    end
+
+    context "saves password properly" do 
+      it "encrypts the password using BCrypt" do 
+        expect(BCrypt::Password).to receive(:create)
+        User.new(username: "mary", password: "lamb")
+      end
+    end
   end
 
 
