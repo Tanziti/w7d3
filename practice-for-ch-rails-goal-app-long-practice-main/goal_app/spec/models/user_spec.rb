aee8a1529dbb760fe86091dc8df96d71be1e835f
tanziti <tanzitian3@gmail.com>
2023-08-09 14:33:17 -0400

    midwayuserspec

--- practice-for-ch-rails-goal-app-long-practice-main/goal_app/spec/models/user_spec.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/goal_app/spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {User.create!(username: "Dennis", password: "password")} #checking if we can create user
+
+  it {should validate_presence_of(:username)}
+  it {should validate_presence_of(:session_token)}
+  it {should validate_presence_of(:password_digest)}
+  it {should validate_length_of(:password).is_at_least(6)}
+  it {should validate_uniqueness_of(:username)}
+  it {should validate_uniqueness_of(:session_token)}
 end
