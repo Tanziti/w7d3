da1c1720565798217ded1d3378e68910278a7137
tanziti <tanzitian3@gmail.com>
2023-08-09 14:34:58 -0400

    midwayuserspec

--- practice-for-ch-rails-goal-app-long-practice-main/goal_app/spec/models/user_spec.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/goal_app/spec/models/user_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe User, type: :model do
   it {should validate_length_of(:password).is_at_least(6)}
   it {should validate_uniqueness_of(:username)}
   it {should validate_uniqueness_of(:session_token)}
+
+
+  describe "finds users by credentials" do
+    context "with a valid username and password" do
+      it "returns the proper user" do 
+        zi = User.create(username: "zi", password: "wordpass")
+        user = User.find_by_credentials("zi", "wordpass")
+        expect(zi.username).to eq(user.username)
+        expect(zi.password_digest).to eq(user.password_digest)
+      end
+
+      
+    end
+
+
+  end
+
+
 end
+
+
